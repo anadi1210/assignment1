a8a62c94319bfe2df4e061a1a703f307b8031cb9
anadi <sharma_anadi@ymail.com>
2021-04-25 16:56:15 -0500

    Main app and savings account class

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -2,4 +2,42 @@ package com.meritamerica.assignment1;
 
 public class MeritAmericaBankApp {
 	
+	public static void main(String[] args) {
+		AccountHolder accountHolder1 = new AccountHolder("Abc", "Def", "Ghi", "123-456-7890",100,1000);
+		
+		System.out.println(accountHolder1);
+		
+		CheckingAccount checkingAccount1 = accountHolder1.getCheckingAccount();
+		SavingsAccount savingsAccount1 = accountHolder1.getSavingsAccount();
+		boolean flag1 = checkingAccount1.deposit(500);
+		if(!flag1) {
+			System.out.println("Incorrect amount entered for deposit");
+		}
+		boolean flag2 = savingsAccount1.withdraw(800);
+		if(!flag2) {
+			System.out.println("Incorrect amount entered for withdraw");
+		}
+		System.out.println("---------------------------");
+		System.out.println(checkingAccount1);
+		System.out.println("---------------------------");
+		System.out.println(savingsAccount1);
+		System.out.println("---------------------------");
+		AccountHolder accountHolder2 = new AccountHolder("John", "James", "Doe","985-746-2312",200,500);
+		
+		
+		
+		CheckingAccount checkingAccount2 = accountHolder2.getCheckingAccount();
+		SavingsAccount savingsAccount2 = accountHolder2.getSavingsAccount();
+		
+		boolean flag3 = checkingAccount2.deposit(-500);
+		if(!flag3) {
+			System.out.println("Incorrect amount entered for deposit");
+		}
+		boolean flag4 = savingsAccount2.withdraw(600);
+		if(!flag4) {
+			System.out.println("Incorrect amount entered for withdraw");
+		}
+		System.out.println("---------------------------");
+		System.out.println(accountHolder2);
+	}
 }
--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -1,5 +1,74 @@
 package com.meritamerica.assignment1;
 
+/*
+this class is defined for user's Savings Account
+
+ */
+
 public class SavingsAccount {
 	
+	private double openingBalance;
+	private double balance;
+	private double interestRate = 0.01;
+	
+	public SavingsAccount() {
+	
+	}
+	 SavingsAccount(double openingBalance){
+		 this.balance = openingBalance;
+	 }
+	
+	 public double getOpeningBalance() {
+		return openingBalance;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	 // This method  will receive the amount to be withdrawn and will return boolean value as output
+	boolean withdraw(double amount) {
+		boolean flag = false;
+		if(amount != 0 && amount <= balance) {
+			balance -= amount;
+			flag = true;
+		}
+		
+		return flag;
+	}
+	// This method  will receive the amount to be deposited and will return boolean value as output.
+	boolean deposit(double amount) {
+		boolean flag = false;
+		if(amount >= 0.0) {
+			balance += amount;
+			flag = true;
+		}
+		return flag;
+	}
+	
+	// this method will calculate the future value for the number of years passed as an argument
+	double futureValue(int years) {
+		double futureAmount = 0.0;
+		if(years >=1) {
+			futureAmount = balance * (Math.pow(1 + interestRate,years));
+		}
+		else
+			System.out.println("Please enter valid value for number of years");
+		return futureAmount;
+	}
+	 // to display the output for the object in specified format
+	@Override
+		public String toString() {
+			
+			return "Savings Account Balance: " + getBalance() + "\n" +
+					"Savings Account Interest Rate: " + getInterestRate() + "\n" +
+					"Savings Account Balance in 3 years: " + futureValue(3);
+		}
 }
